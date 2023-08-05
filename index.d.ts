3f05a4b14fffbbc2b540f5f413c430cd807612f6
qoyf <qoyf@vip.qq.com>
2023-08-05 22:47:02 +0800

    public routers

--- index.d.ts
+++ index.d.ts
@@ -4,9 +4,10 @@ import { IMiddleware } from 'koa-router'
 interface RouterPlus extends Router {
   namespace(prefix: string, ...middlewares: IMiddleware<any, Context>[]): Router;
 }
- 
+
 declare module 'egg' {
   interface Application {
     router: RouterPlus;
+    routers: Array<Array<String | Router>>;
   }
 }
