e4aa19cedf74d086840d915a3abcb578a80b3a91
求解 <qoyf@vip.qq.com>
2023-08-05 23:39:14 +0800

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -3,12 +3,11 @@ import { IMiddleware } from 'koa-router'
 
 interface RouterPlus extends Router {
   namespace(prefix: string, ...middlewares: IMiddleware<any, Context>[]): Router;
-  routers: Array<Array<String | Router>>;
+  routers():Array<Array<String | Router>>;
 }
 
 declare module 'egg' {
   interface Application {
     router: RouterPlus;
-   
   }
 }
